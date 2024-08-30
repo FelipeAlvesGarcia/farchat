99057a6a200fe16bbc57d4959fe85682e1f306be
felipeAG <=>
2024-08-29 21:40:40 -0300

    responsível

--- src/components/Home.js
+++ src/components/Home.js
@@ -16,15 +16,15 @@ function Home (){
                 <div className="row justify-content-center">
                     <div className="col-12 col-sm-10 col-md-8 col-lg-7 justify-content-center">
                         <h1 className="text-center mt-5 mb-5"><a href="/sobre" className="a hover2" id="titulo">FARCHAT</a></h1>
-                        <div className="w-100 p-5 row bg1 justify-content-center m-auto" style={{borderRadius:"25px"}}>
+                        <div className="w-100 p-5 px-3 px-sm-5 row bg1 justify-content-center m-auto" style={{borderRadius:"25px"}}>
                             <p className="text-white mb-2 ms-4 text-start">Nome:</p>
                             <input type="text" className="rounded-pill bg2 border-0 py-3 px-5 fs-6 text-white mb-4" style={{outline: "none"}} placeholder="Nome..."/>
-                            <a href="/salas" className="a d-inline col-12 col-sm-7 col-md-5 col-lg-4  my-5" id="entrarSalas"><img src={botao}className="w-100 hover" alt=""/></a>
+                            <a href="/salas" className="a d-inline col-8 col-sm-6 col-md-5 col-lg-4  my-5" id="entrarSalas"><img src={botao}className="w-100 hover" alt=""/></a>
                         </div>
                     </div>
                 </div>
             </div>
-            <div id="imgBg">
+            <div id="imgBg" className="d-none d-sm-block">
                 <img src={aviao} alt="" className="imgFundo" style={{right:"10%", top:"28%"}}/>
                 <img src={aviao} alt="" className="imgFundo" style={{left:"26%", bottom: "12%", transform: "scaleX(-1)"}}/>
                 <img src={mensagem} alt="" className="imgFundo" style={{left:"6.5%", bottom:"35%", transform: "rotate(-55deg)"}}/>
--- src/components/ListaSalas.js
+++ src/components/ListaSalas.js
@@ -35,23 +35,23 @@ function ListaSalas (){
             <div id="mascara"></div>
             <div className="container-fluid mt-4" id="porta">
                 <div className="row justify-content-center">
-                    <div className="col-7 justify-content-center">
+                    <div className="col-12 col-sm-10 col-md-8 col-lg-7 justify-content-center">
                         <h1 className="text-center mt-5 mb-5" id="titulo">Cimol 631A</h1>
-                        <div className="w-100 p-5 row bg1 justify-content-center" style={{borderRadius:"25px"}}>
+                        <div className="w-100 p-5 px-3 px-sm-5 row bg1 justify-content-center m-auto" style={{borderRadius:"25px"}}>
                             <p className="text-white mb-2 ms-4">Senha:</p>
                             <input type="password" className="rounded-pill bg2 border-0 py-3 px-5 fs-6 text-white mb-4" style={{outline: "none"}} placeholder="********"/>
-                            <a href="/mensagens" className="a d-inline col-4 my-5"><img src={botao} className="w-100 hover" alt=""/></a>
+                            <a href="/mensagens" className="a d-inline col-8 col-sm-6 col-md-5 col-lg-4  my-5"><img src={botao} className="w-100 hover" alt=""/></a>
                         </div>
                     </div>
                 </div>
             </div>
 
-            <header className="bg1">
+            <header className="bg1 py-2 py-sm-1">
                 <div className="container-fluid">
                     <div className="row">
                         <h1 className="ps-4 col-6"><a href="/sobre" className="a ms-2 hover2" id="titulo2">FARCHAT</a></h1>
                         <div className="col-6 justify-content-end d-flex align-items-center">
-                            <a href="/"><img src={sair} style={{width:"1.5rem", height:"1.5rem"}} className="me-4 hover2" alt=""/></a>
+                            <a href="/"><img src={sair} style={{width:"1.5rem", height:"1.5rem"}} className="me-4 me-sm-5 hover2" alt=""/></a>
                         </div>
                     </div>
                 </div>
@@ -59,41 +59,41 @@ function ListaSalas (){
 
             <main id="salas">
                 <div className="container mt-5">
-                    <a className="row bg2 rounded a my-2" id="sala" href="/mensagens">
-                        <div className="col-6">
+                    <a className="row bg2 rounded a my-2 mx-1" id="sala" href="/mensagens">
+                        <div className="col-9">
                             <h2 className="text-white fs-5 p-3">Grupo do Fut</h2>
                         </div>
-                        <div className="col-6 d-flex justify-content-end align-items-center">
+                        <div className="col-3 d-flex justify-content-end align-items-center">
                             <div>
                                 <img src={publica} alt="" style={{width:"1.75rem"}} className="me-4"/>
                             </div>
                         </div>
                     </a>
-                    <div className="row position-relative bg2 rounded a my-2 privada" id="sala" onClick={sala}>
-                        <div className="col-6">
+                    <div className="row position-relative bg2 rounded a my-2 privada mx-1" id="sala" onClick={sala}>
+                        <div className="col-9">
                             <h2 className="text-white fs-5 p-3">Cimol 631A</h2>
                         </div>
-                        <div className="col-6 d-flex justify-content-end align-items-center">
+                        <div className="col-3 d-flex justify-content-end align-items-center">
                             <div>
                                 <img src={privada} alt="" style={{width:"1.75rem"}} className="me-4"/>
                             </div>
                         </div>
                     </div>
-                    <a className="row bg2 rounded a my-2" id="sala" href="/mensagens">
-                        <div className="col-6">
+                    <a className="row bg2 rounded a my-2  mx-1" id="sala" href="/mensagens">
+                        <div className="col-9">
                             <h2 className="text-white fs-5 p-3">Oficina de Jogos</h2>
                         </div>
-                        <div className="col-6 d-flex justify-content-end align-items-center">
+                        <div className="col-3 d-flex justify-content-end align-items-center">
                             <div>
                                 <img src={publica} alt="" style={{width:"1.75rem"}} className="me-4"/>
                             </div>
                         </div>
                     </a>
-                    <a className="row bg2 rounded a my-2" id="sala" href="/mensagens">
-                        <div className="col-6">
+                    <a className="row bg2 rounded a my-2  mx-1" id="sala" href="/mensagens">
+                        <div className="col-9">
                             <h2 className="text-white fs-5 p-3">Torneio de RL</h2>
                         </div>
-                        <div className="col-6 d-flex justify-content-end align-items-center">
+                        <div className="col-3 d-flex justify-content-end align-items-center">
                             <div>
                                 <img src={publica} alt="" style={{width:"1.75rem"}} className="me-4"/>
                             </div>
